5dea7466402f4599e58b824ad0685a0113b291f4
khanmahedi <md.mahedihasan1993@gmail.com>
2025-09-05 12:08:03 +0600

    Add APIService with URLSession and Combine to fetch photos

--- PhotoGallerySwiftUI/PhotoGallerySwiftUI/Services/APIService.swift
+++ PhotoGallerySwiftUI/PhotoGallerySwiftUI/Services/APIService.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Combine
+
+class APIService {
+    static let shared = APIService()
+    
+    func fetchPhotos() -> AnyPublisher<[Photo], Error> {
+        guard let url = URL(string: "https://picsum.photos/v2/list?page=1&limit=30") else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [Photo].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
+
+
