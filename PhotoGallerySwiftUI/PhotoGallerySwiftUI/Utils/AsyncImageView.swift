5f7da817813c38f5570c0587872a9dc25c8b8bf7
khanmahedi <md.mahedihasan1993@gmail.com>
2025-09-05 17:28:34 +0600

    added image showing fade in animation

--- PhotoGallerySwiftUI/PhotoGallerySwiftUI/Utils/AsyncImageView.swift
+++ PhotoGallerySwiftUI/PhotoGallerySwiftUI/Utils/AsyncImageView.swift
@@ -10,18 +10,21 @@ import SwiftUI
 struct AsyncImageView: View {
     @StateObject private var loader = ImageLoader()
     let url: URL?
-    
+    @State private var isLoaded: Bool = false
     var body: some View {
         Group {
             if let image = loader.image {
                 Image(uiImage: image)
                     .resizable()
                     .scaledToFill()
+                    .opacity(isLoaded ? 1 : 0)         
+                    .animation(.easeIn(duration: 0.3), value: isLoaded)
+                    .onAppear { isLoaded = true }
             } else {
                 ProgressView()
             }
         }
-        .frame(height: 150)
+        .frame(height: 150.scaleRespectToHeight())
         .clipped()
         .onAppear {
             if let url = url{
