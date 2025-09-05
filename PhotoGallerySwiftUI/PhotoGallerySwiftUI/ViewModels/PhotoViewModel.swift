209f34a67146b8eefedd72ca1916b16213ba339b
khanmahedi <md.mahedihasan1993@gmail.com>
2025-09-05 12:17:38 +0600

    Add PhotoViewModel with @Published properties and Combine subscription

--- PhotoGallerySwiftUI/PhotoGallerySwiftUI/ViewModels/PhotoViewModel.swift
+++ PhotoGallerySwiftUI/PhotoGallerySwiftUI/ViewModels/PhotoViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Combine
+
+class PhotoViewModel : ObservableObject{
+    @Published var photos: [Photo] = []
+    @Published var errorMessage: String? = nil
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    func loadPhotos() {
+          APIService.shared.fetchPhotos()
+              .sink { completion in
+                  switch completion {
+                  case .failure(let error):
+                      self.errorMessage = error.localizedDescription
+                  case .finished:
+                      break
+                  }
+              } receiveValue: { photos in
+                  self.photos = photos
+              }
+              .store(in: &cancellables)
+      }
+}
